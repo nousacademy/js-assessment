e620ab70adeebd513f624840574eea4be1f1bbdc
Labs-Moov <alessandro.elkan@borderfree.com>
2016-03-04 17:19:25 -0500

    last

--- app/regex.js
+++ app/regex.js
@@ -9,25 +9,31 @@ exports.regexAnswers = {
     },
 
     containsRepeatingLetter: function(str) {
-      var patt = new RegExp(/([a-zA-Z]).*?\1/);
-      var res = patt.test(str);
-      return res;
+        var patt = new RegExp(/([a-zA-Z]).*?\1/);
+        var res = patt.test(str);
+        return res;
     },
 
     endsWithVowel: function(str) {
-      var patt = new RegExp(/[aeiou]([bcdfghjklmnpqrstvwxyz]|nt)$/);
-      var res = patt.test(str);
-      return res;
+        var patt = new RegExp(/[aeiou]$/i);
+        var res = patt.test(str);
+        return res;
     },
 
     captureThreeNumbers: function(str) {
-
+        var patt = new RegExp(/[0-9]{3}/);
+        var res = patt.match(str);
+        return res;
     },
 
     matchesPattern: function(str) {
-
+        var patt = new RegExp(/^\d{3}-\d{3}-\d{4}$/i);
+        var res = patt.test(str);
+        return res;
     },
     isUSD: function(str) {
-
+        var patt = new RegExp(/^\$\d{1,3}(,\d{3})*(\.\d{2})?$/i);
+        var res = patt.test(str);
+        return res;
     }
 };
